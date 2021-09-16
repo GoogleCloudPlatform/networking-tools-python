b17a22b90407212acbd56046a1aa022397a734eb
Sampath Kumar <sam1990kumar@gmail.com>
2021-09-16 15:44:08 +0530

    Update cidr.py

--- cidr.py
+++ cidr.py
@@ -12,74 +12,53 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import print_function
+
 import json
-import sys
+try:
+    from urllib import urlopen
+except ImportError:
+    from urllib.request import urlopen
+    from urllib.error import HTTPError
 
 import netaddr
 
-try:
-    import urllib.request
-except ImportError:
-    import urllib
-    import urllib2
-
-goog_url = "https://www.gstatic.com/ipranges/goog.json"
-cloud_url = "https://www.gstatic.com/ipranges/cloud.json"
-
-
-def read_url_py2(url):
-    try:
-        return urllib.urlopen(url).read()
-    except urllib2.HTTPError:
-        print("Invalid HTTP response from %s" % url)
-        return {}
-
-
-def read_url_py3(url):
-    try:
-        return urllib.request.urlopen(url).read()
-    except urllib.error.HTTPError:
-        print("Invalid HTTP response from %s" % url)
-        return {}
+IPRANGE_URLS = {
+    'goog':  'https://www.gstatic.com/ipranges/goog.json',
+    'cloud': 'https://www.gstatic.com/ipranges/cloud.json',
+}
 
 
 def read_url(url):
     try:
-        if sys.version_info.major == 3:
-            json_text = read_url_py3(url)
-        else:
-            json_text = read_url_py2(url)
-        return json.loads(json_text)
+        return json.loads(urlopen(url).read())
+    except (IOError, HTTPError):
+        print('ERROR: Invalid HTTP response from %s' % url)
     except json.decoder.JSONDecodeError:
-        print("Could not parse HTTP response from %s" % url)
-        return {}
+        print('ERROR: Could not parse HTTP response from %s' % url)
 
 
-def get_ip_address(url):
-    json_data = read_url(url)
-    network_cidrs = {
-        "ipv4": netaddr.IPSet(),
-        "ipv6": netaddr.IPSet(),
-    }
-    if json_data:
-        print("{} published: {}".format(url, json_data.get("creationTime")))
-        for e in json_data["prefixes"]:
-            if e.get("ipv4Prefix"):
-                network_cidrs["ipv4"].add(e.get("ipv4Prefix"))
-            if e.get("ipv6Prefix"):
-                network_cidrs["ipv6"].add(e.get("ipv6Prefix"))
-    return network_cidrs
+def get_data(link):
+    data = read_url(link)
+    if data:
+        print('{} published: {}'.format(link, data.get('creationTime')))
+        cidrs = netaddr.IPSet()
+        for e in data['prefixes']:
+            if 'ipv4Prefix' in e:
+                cidrs.add(e.get('ipv4Prefix'))
+            if 'ipv6Prefix' in e:
+                cidrs.add(e.get('ipv6Prefix'))
+        return cidrs
 
 
 def main():
-    print("Python version " + sys.version)
-    goog_cidrs = get_ip_address(goog_url)
-    cloud_cidrs = get_ip_address(cloud_url)
-    print("IP ranges for Google APIs and services default domains:")
-    for key in ["ipv4", "ipv6"]:
-        for i in goog_cidrs[key].difference(cloud_cidrs[key]).iter_cidrs():
-            print(i)
+    cidrs = {group: get_data(link) for group, link in IPRANGE_URLS.items()}
+    if len(cidrs) != 2:
+        raise ValueError('ERROR: Could process data from Google')
+    print('IP ranges for Google APIs and services default domains:')
+    for ip in (cidrs['goog']-cidrs['cloud']).iter_cidrs():
+        print(ip)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
